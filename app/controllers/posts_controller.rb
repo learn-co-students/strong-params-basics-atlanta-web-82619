b544062c4ab1f27223994eccb0c77f757a92066a
Justina Kuchta <justyna.kuchta@gmail.com>
2019-09-22 10:41:26 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,18 +12,24 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
+	  #@post = Post.new(params["post"]) #we get rid of this so we can specify which parameters are allowed
+	  @post = Post.new(post_params(:title, :description)) #specifies which parameters are allowed
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
+	  @post = Post.find(params[:id]) 
+	  @post.update(post_params(:title)) #specifies which parameters are allowed to be edited
+	  #@post.update(params["post"]) #we get rid of this so we can specify which parameters are allowed
 	  redirect_to post_path(@post)
 	end
 
 	def edit
 	  @post = Post.find(params[:id])
+	end
+
+	def post_params(*args)
+		params.require(:post).permit(*args)
 	end
 end
